52d8c0b6aceb0fe52484f22d4aaf4410df3de0a2
jonathan <jonathan.developpeur@gmail.com>
2022-06-13 19:59:19 +0200

    set_up_gitlab-ci+add_submodules+update_project-versions+set_up_open-feign

---
+++ TourGuide/src/main/java/tourGuide/beans/VisitedLocationBean.java
@@ -0,0 +1,16 @@
+package tourGuide.beans;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class VisitedLocationBean {
+    public final UUID userId;
+    public final tourGuide.beans.LocationBean LocationBean;
+    public final Date timeVisited;
+
+    public VisitedLocationBean(UUID userId, LocationBean LocationBean, Date timeVisited) {
+        this.userId = userId;
+        this.LocationBean = LocationBean;
+        this.timeVisited = timeVisited;
+    }
+}
